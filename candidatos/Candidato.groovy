9121f24e10e3db58f92b10cf1414bebc0b83c5b3
EloiseHelena <eloisehelena2014@gmail.com>
2024-09-15 22:00:20 -0300

    A classe Candidato extende Pessoa e adiciona atributos específicos para candidatos.

--- candidatos/Candidato.groovy
+++ candidatos/Candidato.groovy
@@ -1,4 +1,34 @@
 package candidatos
+import pessoas.Pessoa
 
-class Candidato {
+public class Candidato extends Pessoa {
+    private String cpf;
+    private int idade;
+    private String estado;
+    private String cep;
+
+    public Candidato(String nome, String email, String descricao, String[] competencias,
+                     String cpf, int idade, String estado, String cep) {
+        super(nome, email, descricao, competencias);
+        this.cpf = cpf;
+        this.idade = idade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
 }
