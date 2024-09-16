482269217f8e0cd897412ff0a996fc033fce1494
EloiseHelena <eloisehelena2014@gmail.com>
2024-09-15 22:09:02 -0300

    A classe Empresa também extende Pessoa, mas é voltada para empresas.

--- empresas/Empresa.groovy
+++ empresas/Empresa.groovy
@@ -1,4 +1,34 @@
 package empresas
+import pessoas.Pessoa
 
-class Empresa {
+class Empresa extends Pessoa {
+    String cnpj
+    String pais
+    String estado
+    String cep
+
+    Empresa(String nome, String email, String descricao, List<String> competencias,
+            String cnpj, String pais, String estado, String cep) {
+        super(nome, email, descricao, competencias)
+        this.cnpj = cnpj
+        this.pais = pais
+        this.estado = estado
+        this.cep = cep
+    }
+
+    String getCnpj() {
+        return cnpj
+    }
+
+    String getPais() {
+        return pais
+    }
+
+    String getEstado() {
+        return estado
+    }
+
+    String getCep() {
+        return cep
+    }
 }
