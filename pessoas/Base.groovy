4094944ed0efdfec433f7c4b322829c31fa7b450
EloiseHelena <eloisehelena2014@gmail.com>
2024-09-15 22:01:26 -0300

    Reorganização da Interface de Base, deixando apenas os itens necessários.

--- pessoas/Base.groovy
+++ pessoas/Base.groovy
@@ -4,12 +4,6 @@ interface Base {
     public interface Pessoa {
         String getNome();
         String getEmail();
-        String getCpf();
-        String getCnpj();
-        String getIdade();
-        String getEstado();
-        String getPais();
-        String getCep();
         String getDescricao();
         String[] getCompetencias();
     }
