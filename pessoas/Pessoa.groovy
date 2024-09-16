dbd2ef415a462d6a39c9a4bb09948f3822f8bc68
EloiseHelena <eloisehelena2014@gmail.com>
2024-09-15 21:33:09 -0300

    Essa classe abstrata implementa a interface Pessoa com atributos e métodos comuns e serve de base para Candidato e Empresa.

--- pessoas/Pessoa.groovy
+++ pessoas/Pessoa.groovy
@@ -1,4 +1,78 @@
 package pessoas
 
-class Pessoa {
+public abstract class Pessoa implements Base {
+    protected String nome;
+    protected String email;
+    protected String cpf;
+    protected String cnpj;
+    protected String idade;
+    protected String estado;
+    protected String pais;
+    protected String cep;
+    protected String descricao;
+    protected String[] competencias;
+
+    public Pessoa(String nome, String email, String cpf, String cnpj, String idade, String estado, String pais, String cep,String descricao, String[] competencias) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.cnpj = cnpj;
+        this.idade = idade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+        this.descricao = descricao;
+        this.competencias = competencias;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getCpf() {
+        return cpf;
+    }
+
+    @Override
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    @Override
+    public String getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String getEstado() {
+        return estado;
+    }
+
+    @Override
+    public String getPais() {
+        return pais;
+    }
+
+    @Override
+    public String getCep() {
+        return cep;
+    }
+
+    @Override
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String[] getCompetencias() {
+        return competencias;
+    }
 }
+
